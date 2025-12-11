48ff35069fa41b60b30c871b1a402754d740d645
vinayak <vinayak.jaiswal24@gmail.com>
2025-12-11 15:47:03 +0530

    initial commit

---
+++ src/main/java/com/mycompany/location_management_api/LocationManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.location_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/location_management_api/LocationManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.location_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
